530780771ebfd7508e61fef2dbfc036a275c3b9d
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-08-25 12:43:09 +0200

    Server files

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const bodyParser = require("body-parser")
+const moment = require("moment")
 
+
+const generalRouter = require("./src/routes/generalRoutes")
+const usersRouter = require('./src/routes/userRoutes')
+
+mongoose.connect(process.env['MONGOOSE_URL'], { useNewUrlParser: true },  { useUnifiedTopology: true })
+
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+
+app.use('/', generalRouter)
+app.use('/api/users', usersRouter)
 
 
+
+
+let User = mongoose.model("User", userSchema)
+let Exercise = mongoose.model("Exercise", exerciseSchema)
 
 
 
