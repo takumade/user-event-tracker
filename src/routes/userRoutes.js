a55b881fe0c4d1bdc16105efe271076ffec3501f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-08-25 20:50:17 +0200

    modify user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,9 +7,7 @@ const userCtrl = new UserController();
 router.post("/", userCtrl.createUser);
 router.get("/", userCtrl.getUsers);
 router.get("/:_id", userCtrl.getUser);
-router.post("/:_id/events", userCtrl.getUserEvents);
 router.patch("/:_id", userCtrl.editUser);
 router.post("/:_id/edit", userCtrl.editUser);
-router.get("/:_id/logs", userCtrl.getUserLogs);
 
 module.exports = router;
