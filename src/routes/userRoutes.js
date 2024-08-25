cb6ff7fe75ec41b5936fa5d6367a9884bb2345b4
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-08-25 13:10:26 +0200

    Fix log bugs

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,9 +4,9 @@ const UserController = require("../controllers/userController");
 const router = express.Router();
 const userCtrl = new UserController();
 
-router.post("/api/users", userCtrl.createUser);
-router.get("/api/users", userCtrl.getUsers);
-router.post("/api/users/:_id/exercises", userCtrl.getUserExercises);
-router.get("/api/users/:_id/logs", userCtrl.getUserLogs);
+router.post("/", userCtrl.createUser);
+router.get("/", userCtrl.getUsers);
+router.post("/:_id/exercises", userCtrl.getUserExercises);
+router.get("/:_id/logs", userCtrl.getUserLogs);
 
 module.exports = router;
