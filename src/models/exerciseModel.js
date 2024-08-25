980ae5167fb595fee14d30125c5a39b9af2db819
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-08-25 12:42:29 +0200

    Add exercise model

---
+++ src/models/exerciseModel.js
@@ -0,0 +1,8 @@
+exerciseSchema = new Schema({
+    userId: {type:String, required: true},
+    date: {type:Date, required: true},
+    duration: Number,
+    description: {type:String, required: true}
+ })
+ 
+ 
