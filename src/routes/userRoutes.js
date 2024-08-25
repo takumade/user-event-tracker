07311c7584d1e61d13a12803b499a9343e49dd6e
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-08-25 19:36:59 +0200

    Add patch method

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,6 +8,8 @@ router.post("/", userCtrl.createUser);
 router.get("/", userCtrl.getUsers);
 router.get("/:_id", userCtrl.getUser);
 router.post("/:_id/events", userCtrl.getUserEvents);
+router.patch("/:_id", userCtrl.editUser);
+router.post("/:_id/edit", userCtrl.editUser);
 router.get("/:_id/logs", userCtrl.getUserLogs);
 
 module.exports = router;
